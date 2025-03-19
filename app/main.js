a45f132a16abe7d79b133387e94dccb5e245275a
hthakur <hthakur@microproindia.com>
2025-03-19 10:57:58 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -10,12 +10,12 @@ const rl = readline.createInterface({
     completer: completer,
     prompt: "$ ",
 });
-let lastTabInput = "''";
+let lastTabInput = "";
 let tabPressCount = 0;
 function longestCommonPrefix(strings) {
     if (strings.length === 0) return "";
     let prefix = strings[0];
-    for (let i = 1; i < strings.length; i++) {
+    for (let i = 1; i <= strings.length; i++) {
         while (strings[i].indexOf(prefix) !== 0) {
             prefix = prefix.slice(0, -1);
             if (prefix === "") return "";
