ad9a61c6a55501f84126b9e9e0a5ccd456fbaee6
hthakur <hthakur@microproindia.com>
2025-03-19 10:58:39 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -15,10 +15,10 @@ let tabPressCount = 0;
 function longestCommonPrefix(strings) {
     if (strings.length === 0) return "";
     let prefix = strings[0];
-    for (let i = 1; i <= strings.length; i++) {
+    for (let i = 1; i < strings.length; i++) {
         while (strings[i].indexOf(prefix) !== 0) {
             prefix = prefix.slice(0, -1);
-            if (prefix === "") return "";
+            if (prefix === "") return ;
         }
     }
     return prefix;
