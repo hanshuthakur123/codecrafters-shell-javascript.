1dfaa8eb3376fd3fd70879973eb781733336180c
hthakur <hthakur@microproindia.com>
2025-03-20 15:15:19 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -138,6 +138,10 @@ function repeat() {
 function completer(line) {
   const completions = getMatchingCommands(line);
   const hits = completions.filter((c) => c.startsWith(line));
+  if (hits.length === 1) {
+    // If there's only one match, append a space after the autocompleted command
+    return [[hits[0] + ' '], line];
+  }
   return [hits.length ? hits : completions, line];
 }
 
