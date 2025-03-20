df8385f938643b6f4bcbb4f45d9364b6954b8ee8
hthakur <hthakur@microproindia.com>
2025-03-20 15:37:46 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -146,7 +146,7 @@ function completer(line) {
 
   if (hits.length === 1) {
     // If there's only one match, append a space after the autocompleted command
-    return [[hits[0]+ "' '" ], line];
+    return [[hits[0]+ ' ' ], line];
   }
 
   // If there are multiple matches, ring the bell and return the list of matches
