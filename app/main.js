2482dda7ce783a201ee59273d606b0226aaa70f6
hthakur <hthakur@microproindia.com>
2025-03-21 17:58:05 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -30,7 +30,7 @@ function completer(line) {
     if (isFirstTabPress) {
       // First tab press: ring the bell and do not autocomplete
       process.stdout.write('\x07'); // Ring the bell
-      isFirstTabPress = false;
+      
       return [hits[0], line];
     } else {
       // Second tab press: list all completions on a single line
