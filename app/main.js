9d583cb8bacb3e0fe1722bac939af4be58594a8a
hthakur <hthakur@microproindia.com>
2025-03-24 17:38:44 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -166,7 +166,7 @@ function completer(line) {
 
   if (hits.length === 1) {
     // If there's only one match, append a space after the autocompleted command
-    return [[hits[0] + ' '], line];
+    return [[hits[0] ], line];
   }
 
   // Find the common prefix among all matches
