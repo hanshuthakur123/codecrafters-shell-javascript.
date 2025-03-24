c255127ef2317682bd1102948e7232d0c9430559
hthakur <hthakur@microproindia.com>
2025-03-24 18:08:49 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -168,7 +168,7 @@ function completer(line) {
  
   if (hits.length === 1  && hits[0] === line) {
     // If there's only one match, append a space after the autocompleted command
-    return [[hits[0]], line];
+    return [[hits[0]+' '], line];
   }
   if (hits.length === 1 && hits[0].startsWith(line)) {
     // If there's only one match, append a space after the autocompleted command
