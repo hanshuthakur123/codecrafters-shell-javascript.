3d9cef05c96139d9fc06fc2ce52d69f0551612d7
hthakur <hthakur@microproindia.com>
2025-03-24 17:15:22 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -146,12 +146,12 @@ function completer(line) {
 
   if (hits.length === 1) {
     // If there's only one match, append a space after the autocompleted command
-    return [[hits[0]+' '], line+' '];
+    return [[hits[0]+' '], line];
   }
 
   // If there are multiple matches, ring the bell and return the list of matches
   process.stdout.write('\x07'); // Ring the bell
-  return [hits, line]; // Return the list of completions without modifying the prompt
+  return [hits+' ', line]; // Return the list of completions without modifying the prompt
 }
 
 function getMatchingCommands(line) {
