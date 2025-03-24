f15f45c5c1c9c055f4d93e9c49d9f9ab61874b1c
hthakur <hthakur@microproindia.com>
2025-03-24 16:49:23 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -150,7 +150,7 @@ function completer(line) {
   }
 
   // If there are multiple matches, ring the bell and return the list of matches
-  process.stdout.write('\a'); // Ring the bell
+  process.stdout.write('\x07'); // Ring the bell
   return [hits, line]; // Return the list of completions without modifying the prompt
 }
 
