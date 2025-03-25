78edc6341b02813ae5d24c6c6266b813754b2076
hthakur <hthakur@microproindia.com>
2025-03-25 11:01:01 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -159,7 +159,8 @@ function completer(line) {
   const allCommands = getMatchingCommands(line);
   // Use Set to remove duplicates
   const uniqueCommands = [...new Set(allCommands)];
-  const hits = uniqueCommands.filter((c) => c.startsWith(line));
+  const hits = uniqueCommands.filter((c) => c.startsWith(line))
+  .sort();
   
   if (hits.length === 0) {
     // No matches, return nothing
